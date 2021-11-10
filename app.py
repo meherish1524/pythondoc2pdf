7b961b1d74a9d31ee4c8a5b08534ee4565015722
manoharvirati <40772700+manoharvirati@users.noreply.github.com>
2021-11-10 16:30:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request,render_template,redirect,url_for,send_file
 import os
-import pywin32
+import win32com.client
 
 import pythoncom
 pythoncom.CoInitialize()
